92a57d079b8bc3bfda21afe4a8803a69c3f2b4b4
Ashraf Zeriouh <azeriouh24@gmail.com>
2026-04-18 08:35:29 -0500

    initial commit

---
+++ build/CMakeFiles/NumberGuess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumberGuess.dir/src/main.cpp.o"
+  "CMakeFiles/NumberGuess.dir/src/main.cpp.o.d"
+  "NumberGuess.exe"
+  "NumberGuess.pdb"
+  "libNumberGuess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumberGuess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/NumberGuess.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NumberGuess.
+# This may be replaced when dependencies are built.
